279bea8d409c4e61fc3fd62feda527be48f0386c
brcshakil1 <brcshakil72@gmail.com>
2023-11-05 00:12:50 +0600

    created AuthProvider and finished the create user, sign in, google sign in, logout, get user, loading

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBngYdXSRaMkLDryFSxKbK6JXgK4cj-4E8",
+  authDomain: "lets-study-435f5.firebaseapp.com",
+  projectId: "lets-study-435f5",
+  storageBucket: "lets-study-435f5.appspot.com",
+  messagingSenderId: "85115056983",
+  appId: "1:85115056983:web:016d057e62bc8e0cb80412",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,65 @@
+import { createContext, useEffect, useState } from "react";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
+
+export const AuthContext = createContext();
+
+const AuthProvider = () => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const auth = getAuth(app);
+  const googleProvider = new GoogleAuthProvider();
+
+  const createUser = (email, pass) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, pass);
+  };
+
+  const signInUser = (email, pass) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, pass);
+  };
+
+  const googleSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const logout = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setLoading(false);
+      setUser(currentUser);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [auth]);
+
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    signInUser,
+    googleSignIn,
+    logout,
+  };
+
+  return <AuthContext.Provider value={authInfo}></AuthContext.Provider>;
+};
+
+export default AuthProvider;
